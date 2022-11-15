f3f25766a5cf7a65d51c7951b725577adff70664
Dmitry <jdmitrys@yandex.ru>
2022-11-15 23:23:55 +0400

    Create template

---
+++ template.py
@@ -0,0 +1,28 @@
+import pygame
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill(BLACK)
+    pygame.display.flip()
+
+pygame.quit()
